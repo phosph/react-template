80fc12aa5d89daa509af8c69b94f29b0a14d31e7
Jesus Figueredo <jafigueredo@gmail.com>
2022-04-18 12:30:45 -0300

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from './assets/logo.svg';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <div className="text-center">
+    <header
+      className={`
+        min-h-screen
+        flex flex-col items-center justify-center
+        text-white text-[calc(10px_+_2vmin)]
+        bg-[#282c34]
+    `}
+    >
+      <img
+        src={logo}
+        alt="logo"
+        className={`
+            h-[40vmin]
+            pointer-events-none
+            animate-spin [animation-duration:20s]
+        `}
+      />
+      <p>
+        Edit <code>src/App.tsx</code> and save to reload.
+      </p>
+      <a
+        className="color-[#61dafb]"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
+    </header>
+  </div>
+);
 
 export default App;
